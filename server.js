dfeceb611a8f60fbb5c7e03bf1a5373091db6e21
td94 <quynhtrangdo94@gmail.com>
2019-03-09 15:42:47 -0800

    Add new Relic and stress test

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const path = require('path');
-const favicon = require('serve-favicon')
+const favicon = require('serve-favicon');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
@@ -12,14 +14,21 @@ app.use(compression());
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.use('/files', express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
-app.get('/products/:id', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
-});
+app.use('/products/:id', express.static(path.join(__dirname, 'public')));
+
+// app.use('/restaurants/:id', proxy({target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com/'}));
+// app.use('/articles/:id', proxy({target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com/'}));
+// app.use('/reviews/:id', proxy({target: 'http://ec2-3-92-226-206.compute-1.amazonaws.com/'}));
+app.use('/api/products', proxy({target: 'http://localhost:3000'}));
+
+// app.get('/products/:id', (req, res) => {
+//   res.sendFile(`${__dirname}/public/index.html`);
+// });
 
 app.get('*', (req, res) => {
   res.redirect('/products/1');
 })
 
-app.listen(80);
+app.listen(4020);
